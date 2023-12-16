a9e06da275ad64fef58aea77615eaba18c77ee2e
Kauan-Henrique-de-Souza <kauanhenrique5696@gmail.com>
2023-12-16 18:06:04 -0300

    Update

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ function imprimirDados(dados){
     document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
     document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "ºC"
     document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
-    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
     document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
 }
 
