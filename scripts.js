0e17e0c6cf6c3ee04d4834dff09488248365599b
Kauan-Henrique-de-Souza <kauanhenrique5696@gmail.com>
2023-12-15 22:31:42 -0300

    Add functions Javascript

--- scripts.js
+++ scripts.js
@@ -1,9 +1,20 @@
 const key = "8149313b67dd53e6a639d43102a828c0"
 
-// buscar a informaçao na API
+//Busca os dados na API e imprimi na tela
+function imprimirDados(dados){
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "ºC"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+// busca a informaçao na API
 async function buscarCidade(cidade){
 
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}"`).then(resposta => resposta.json())
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+
+    imprimirDados(dados)
 }
 
 // funcao botao
